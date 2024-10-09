a0e4df74056d8293d3352a8b7f501447a335263e
shivamjha2054 <145041172+shivamjha2054@users.noreply.github.com>
2024-10-09 21:28:26 +0530

    Add files via upload

---
+++ day12.FLIPCARDS.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T; 
+    while (T--) {
+        int N, X;
+        cin >> N >> X;
+        
+        cout << min(X, N - X) << endl;
+    }
+    return 0;
+}
