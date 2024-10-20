cce389c56d81294ff39d69b10c7f71af1faddd08
shivamjha2054 <145041172+shivamjha2054@users.noreply.github.com>
2024-10-20 16:43:23 +0530

    Add files via upload

---
+++ day18.BIN_BAT.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    
+    while (T--) {
+        int N, A, B;
+        cin >> N >> A >> B;
+        
+        int rounds = log2(N);
+        
+        int total_time = (rounds * A) + (rounds - 1) * B;
+        
+        cout << total_time << endl;
+    }
+    
+    return 0;
+}
